1b782aace6c122d13a137a218069681d20897e0e
Sebastijan Kokai <sebastijan.kokai@outlook.com>
2023-09-29 14:19:31 +0200

    Add Room dependency to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,10 @@ android {
 }
 
 dependencies {
+    val roomVersion = "2.5.2"
+
+    implementation ("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor ("androidx.room:room-compiler:$roomVersion")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
